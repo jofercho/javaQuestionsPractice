ee8d3ef78a7cd203f77bdfa94984fb9daf3eed57
jofercho <ssoj.josss@gmail.com>
2022-05-16 11:32:54 -0500

    min in rotated sorted array

---
+++ src/test/java/FindMinInSortedRotatedTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindMinInSortedRotatedTest {
+
+    @Test
+    public void findMinInSortedRotatedTest(){
+        int[] input1 = new int[]{3,4,5,1,2};
+        int[] input2 = new int[]{4,5,6,7,0,1,2};
+        int[] input3 = new int[]{11,13,15,17};
+
+        int output1 = 1;
+        int output2 = 0;
+        int output3 = 11;
+        FindMinInSortedRotated min = new FindMinInSortedRotated();
+        Assert.assertEquals(min.findMin(input1), output1);
+        Assert.assertEquals(min.findMin(input2), output2);
+        Assert.assertEquals(min.findMin(input3), output3);
+    }
+}
