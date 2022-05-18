9745fce96c94809039172b4a75141af2323798e9
jofercho <ssoj.josss@gmail.com>
2022-05-18 11:43:20 -0500

    is nunber on lists

---
+++ src/main/java/IsNumberOnTheList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IsNumberOnTheList {
+
+  public List<Integer> isNumberOnTheLists(List<Integer> a, List<Integer> b, List<Integer> c) {
+    List<Integer> solution = new ArrayList<>();
+    Map<Integer, Integer> numbers = new HashMap<>();
+    a.forEach(number -> numbers.put(number, 1));
+    b.forEach(number -> {
+      if (numbers.containsKey(number)) {
+        numbers.put(number, 2);
+      }
+    });
+    c.forEach(number -> {
+      if (numbers.containsKey(number)) {
+        numbers.put(number, 3);
+      }
+    });
+    numbers.forEach((number, repetition) -> {
+      if (repetition == 3) {
+        solution.add(number);
+      }
+    });
+    return solution;
+  }
+}
---
+++ src/test/java/IsNumberOnTheListTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IsNumberOnTheListTest {
+
+  @Test
+  public void isNumberOnTheListTest() {
+    List<Integer> a = Arrays.asList(1, 5, 10, 20, 40, 80);
+    List<Integer> b = Arrays.asList(6, 7, 20, 80, 100);
+    List<Integer> c = Arrays.asList(3, 4, 15, 20, 30, 70, 80, 100, 120);
+
+    int[] output = new int[] { 80, 20 };
+    IsNumberOnTheList isNumberOnTheList = new IsNumberOnTheList();
+    Assert.assertArrayEquals(isNumberOnTheList
+            .isNumberOnTheLists(a, b, c)
+            .stream()
+            .mapToInt(Integer::intValue)
+            .toArray(), output);
+  }
+}
