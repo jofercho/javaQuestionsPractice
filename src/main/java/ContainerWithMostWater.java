3b7689e8aa3274b8e86d2342301666da73c5f5b8
jofercho <ssoj.josss@gmail.com>
2022-05-19 19:57:33 -0500

    ThreeSum

---
+++ src/main/java/ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height){
+        int maxArea = 0;
+        int left =  0;
+        int right = height.length - 1;
+        int maxHeight;
+        while (left < right){
+            maxHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, maxHeight * (right - left));
+            if(height[left]>height[right]){
+                right--;
+            }else {
+                left++;
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/test/java/ContainerWithMostWaterTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ContainerWithMostWaterTest {
+  @Test
+  public void containerWithMostWater() {
+    int[] input1 = new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    int[] input2 = new int[] {1, 1};
+    int output1 = 49;
+    int output2 = 1;
+    ContainerWithMostWater water = new ContainerWithMostWater();
+    Assert.assertEquals(water.maxArea(input1), output1);
+    Assert.assertEquals(water.maxArea(input2), output2);
+  }
+}
